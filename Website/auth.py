3c46f43cb68084643c78cbf94eeaf93ca922cc14
Mackenzy Noble <macnoble@shaw.ca>
2024-02-26 14:16:57 -0400

    Added create account and login raw sql with parameters to remove sql injection

--- Website/auth.py
+++ Website/auth.py
@@ -2,12 +2,33 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from sqlalchemy.sql import text
 
 # setup Blueprint for authentication webpages
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('pswd')
+
+        #usercheck = User.query.filter_by(email=email).first()
+
+        usercheck = db.session.execute(text("SELECT * FROM Users WHERE email = :e"), {'e': email}).fetchone()
+
+        #if usercheck:
+        if usercheck:
+            if check_password_hash(usercheck[2], password):
+                flash("Logged in successfully!", category='success')
+                return redirect('views.home')
+            else:
+                flash("Credentials given did not match our records. Try again.", category='error')
+            #print(usercheck[2])
+        else :
+            flash("Email given does not have an account.", category='error')
+
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -21,7 +42,7 @@ def create_account():
         name = request.form.get('name')
         pswd1 = request.form.get('pswd1')
         pswd2 = request.form.get('pswd2')
-        
+
         if len(email) < 4:
             flash("Email must be greater than 4 characters.", category='error')
         elif len(name) < 2:
@@ -31,11 +52,24 @@ def create_account():
         elif len(pswd1) < 7:
             flash("Password must be secure. Requirements: At least 8 characters and can use lowercase, uppercase, or !@#$%^&()", category='error')
         else:
-            new_user = User(email=email, password=generate_password_hash(pswd1, method='pbkdf2'), name=name)
-            db.session.add(new_user)
-            db.session.commit()
-            flash("Account created!", category='success')
-            # send user home
-            return redirect(url_for('views.home'))
+
+            # check if user exists w/ email already
+            userresult = User.query.filter_by(email=email).first()
+
+            if userresult:
+                flash("User already exists.", category='error')
+            else:
+                #newUser = User(email = email, password = generate_password_hash(pswd1, method='pbkdf2'), name = name)
+                #db.session.add(newUser)
+                #db.session.commit()
+                #flash("Account created!", category='success')
+                # send user home
+                #return redirect(url_for('auth.login'))
+
+                addresult = db.session.execute(text("INSERT INTO Users (email, password, name) VALUES (:e, :p, :n)"), {'e': email, 'p': generate_password_hash(pswd1, method='pbkdf2'), 'n': name})
+                db.session.commit()
+                if addresult.rowcount == 1:
+                    flash("Account created!", category='success')
+                    return redirect(url_for('auth.login'))
                    
     return render_template("create_account.html")
--- Website/models.py
+++ Website/models.py
@@ -3,7 +3,13 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     # have a separate ID as primary key instead of the email due to uniqueness/scecurity
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(50))
-    name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+
+    # FOR DEBUG, REMOVE LATER
+    def __repr__(self):
+        return f'<User {self.name}>'
